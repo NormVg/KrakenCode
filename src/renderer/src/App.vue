e3d9c619db454405ff132fc10e28e1348eaf3b04
Vishnu_Mac <thenormvg@gmail.com>
2026-07-27 17:24:03 +0530

    style: slightly increase app layout top padding for dragging

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -182,7 +182,7 @@ onMounted(async () => {
   min-height: 0;
   position: relative;
   overflow: hidden;
-  padding: 24px 8px 8px 8px; /* Increased top padding for easier dragging */
+  padding: 14px 8px 8px 8px; /* Slightly increased top padding for easier dragging */
   gap: 8px;
 }
 
